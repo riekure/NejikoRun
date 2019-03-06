f1ece384aabbe5093407fc50d7d66bef54148e2d
riekure <s2darl0uvgia24@gmail.com>
2019-03-07 00:11:45 +0900

    process p.257

--- Assets/Scripts/LifePanel.cs
+++ Assets/Scripts/LifePanel.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class LifePanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] icons;
 
-    // Update is called once per frame
-    void Update()
+    // ライフに応じてスプライトを出し分ける
+    public void UpdateLife(int life)
     {
-        
+        for (int i = 0; i < icons.Length; i++)
+        {
+            if (i < life)
+            {
+                icons[i].SetActive(true);
+            }
+            else
+            {
+                icons[i].SetActive(false);
+            }
+        }
     }
 }
