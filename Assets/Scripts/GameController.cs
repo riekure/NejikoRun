49e941a84df04b97934ea0368582b1bfe18ef2b3
riekure <s2darl0uvgia24@gmail.com>
2019-03-09 23:35:22 +0900

    process p.258

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public TextAlignment scoreLabel;
+    public LifePanel lifePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スコアラベルを更新
+        int score = CalcScore();
+        scoreLabel.text = "Score ： " + score + "m";
+    }
+
+    int CalcScore()
+    {
+        // ねじ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
